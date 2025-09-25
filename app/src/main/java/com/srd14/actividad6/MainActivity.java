9db9c234cbd10e149b287851dbc80973842075a6
SebatianRD14 <sebasromo87@gmail.com>
2025-09-24 20:10:24 -0600

    Se hizo la logica del main los xml, imagenes y todo lo necesario

--- app/src/main/java/com/srd14/actividad6/MainActivity.java
+++ app/src/main/java/com/srd14/actividad6/MainActivity.java
@@ -1,6 +1,16 @@
 package com.srd14.actividad6;
 
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Scene;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.transition.TransitionSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +19,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Scene escenaInicial;
+    private Scene escenaFinal;
+    private boolean showingInitial = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ViewGroup sceneRoot = findViewById(R.id.root_escenas);
+        escenaInicial = Scene.getSceneForLayout(sceneRoot, R.layout.escena_1, this);
+        escenaFinal   = Scene.getSceneForLayout(sceneRoot, R.layout.escena_2, this);
+        escenaInicial.enter();
+
+        Button btn = findViewById(R.id.Boton);
+        btn.setOnClickListener(v -> toggleScene(sceneRoot));
+    }
+    private void toggleScene(ViewGroup sceneRoot) {
+        Transition explode = new Explode();
+        Transition slide   = new Slide(Gravity.BOTTOM);
+        Transition fade    = new Fade();
+
+        TransitionSet set = new TransitionSet()
+                .addTransition(explode)
+                .addTransition(fade)
+                .setDuration(300);
+
+        if (showingInitial) {
+            TransitionManager.go(escenaFinal, set);
+        } else {
+            TransitionManager.go(escenaInicial, set);
+        }
+        showingInitial = !showingInitial;
     }
 }
